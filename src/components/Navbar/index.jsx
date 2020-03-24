74e3ce962a5f5de0b9d16263f8a00f0b3fce685a
MuGaber <mohammedgaber273@gmail.com>
2020-03-24 03:05:23 +0200

    added Navbar

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap'
+
+const NavbarComp = () => {
+  return (
+    <Navbar bg='primary' variant='dark'>
+      <Navbar.Brand href='#home'>MMA</Navbar.Brand>
+      <Nav className='mr-auto'>
+        <Nav.Link href='#home'>Home</Nav.Link>
+        <Nav.Link href='#features'>Features</Nav.Link>
+        <Nav.Link href='#pricing'>Pricing</Nav.Link>
+      </Nav>
+      <Form inline>
+        <FormControl type='text' placeholder='Search' className='mr-sm-2' />
+        <Button variant='outline-light'>Search</Button>
+      </Form>
+    </Navbar>
+  )
+}
+
+export default NavbarComp
